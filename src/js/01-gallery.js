6dd1eb923b210da69777f2b34d9eed5c0dfef18b
Oleksandr Vasylenko <79399963+aalexblade@users.noreply.github.com>
2022-10-26 21:17:16 +0300

    package-json-hw-08

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,37 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+const galaryMarkup = createMarckup(galleryItems);
+// console.log(galaryMarkup);
+
+
+gallery.insertAdjacentHTML("beforeend", galaryMarkup);
+
+gallery.addEventListener("click", onGalleryContainerClick);
+
+
+function createMarckup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+   <a class="gallery__item" href="${original}">
+        <img class="gallery__image" 
+        src="${preview}" 
+        alt="${description}" />
+    </a>
+    `;
+    })
+    .join("");
+}
+
+function onGalleryContainerClick(evt) {
+    evt.preventDefault();
+}
+const lightbox = new SimpleLightbox('.gallery__item')
